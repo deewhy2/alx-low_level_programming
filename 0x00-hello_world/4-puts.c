7601464fe6ce640d8ca49e47ab457818cde5e68f
Lawal Mukhtar Oladayo <lawaldone@gmail.com>
2022-06-30 21:34:34 -0700

    dayodee

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,15 @@
 #include <string.h>
 
 /**
- * main
- * function
+ * main - Entry point
  *
- * function descripto
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
+i
