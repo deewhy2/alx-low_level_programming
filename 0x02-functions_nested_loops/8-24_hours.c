39f6f2b9b7c9e7501f6e3520602105ce52dfe377
Lawal Mukhtar Oladayo <lawaldone@gmail.com>
2022-07-05 14:52:15 -0700

    dayodee

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,22 @@
 #include "main.h"
 
+
+
 /**
  * jack_bauer - prints every minute of the day
  * Description: prints every minute of the day
  * Return: void
  */
+
+
+
 void jack_bauer(void)
+
 {
-int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;	
+
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+
+
 
 	hours_max = 58;
 	hours_tens = '0';
