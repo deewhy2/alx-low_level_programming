5dd328ea3cf56ac55ccb4e66326440c88e78607a
Lawal Mukhtar Oladayo <lawaldone@gmail.com>
2022-07-22 17:58:07 -0700

    dde

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,17 @@
-#include "main.h"
-
-/**
- * print_alphabet - Alphabet in lowercase
- *
- * Return:0
- *
- */
-void print_alphabet(void)
-{
-	char r;
-
-	for (r = 'a'; r <= 'z'; r++)
-	{
-		_putchar(r);
-	}
-
-}
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void print_times_table(int n);
+#endif
